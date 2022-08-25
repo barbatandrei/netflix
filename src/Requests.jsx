72ac405c5e22d434c15aff2a87a82aa7f0c1c3e9
barbatandrei <barbatandrei21@gmail.com>
2022-08-25 15:24:09 +0300

    Add API_KEY into .env

--- src/Requests.jsx
+++ src/Requests.jsx
@@ -1,10 +1,7 @@
 const requests = {
-	requestPopular:
-		"https://api.themoviedb.org/3/movie/popular?api_key=cc2c75b2e3fdf50dad5994f2ec121120&language=en-US&page=1",
-	requestTopRated:
-		"https://api.themoviedb.org/3/movie/top_rated?api_key=cc2c75b2e3fdf50dad5994f2ec121120&language=en-US&page=1",
-	requestUpcoming:
-		"https://api.themoviedb.org/3/movie/upcoming?api_key=cc2c75b2e3fdf50dad5994f2ec121120&language=en-US&page=1",
+	requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
+	requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
+	requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_MOVIE_API_KEY}&language=en-US&page=1`,
 };
 
 export default requests;
